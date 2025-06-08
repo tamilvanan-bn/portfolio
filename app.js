1e5e13edfd584732794f03bbd73711ebd6089517
Tamilvanan <tamilvanan.b@outlook.com>
2025-06-08 20:40:11 +0530

    remove cf and modify animations

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-const observer = new IntersectionObserver((entries) => {
-  entries?.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
-    }
-  });
-});
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries?.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      } else {
+        entry.target.classList.remove("show");
+      }
+    });
+  },
+  {
+    threshold: 0.1, // Trigger when at least 10% of the element is visible
+    rootMargin: "0px", // No margin around the viewport
+  }
+);
 
 const hiddenElement = document.querySelectorAll(".hidden");
 hiddenElement?.forEach((el) => observer.observe(el));
+
+const date = new Date().getFullYear();
+document.getElementById("year").innerHTML = date;
